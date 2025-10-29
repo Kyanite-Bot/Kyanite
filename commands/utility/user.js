dac6ba9a017e3feae765f8178295957fd1b14831
Anurag Shre <aquaflames2005@gmail.com>
2025-10-29 21:01:27 +0530

    token hidden

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,14 +1,22 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, time, TimestampStyles} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("user")
     .setDescription("Provides information about the user."),
   async execute(interaction) {
-    // interaction.user is the object representing the User who ran the command
+    // interaction.user is server
     // interaction.member is the GuildMember object, which represents the user in the specific guild
     await interaction.reply(
-      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+      `This command was run by ${
+        interaction.user.username
+      }, who joined the server ${time(
+        interaction.member.joinedAt,
+        TimestampStyles.RelativeTime
+      )} and joined discord ${time(
+        interaction.user.createdAt,
+        TimestampStyles.RelativeTime
+      )};`
     );
   },
 };
